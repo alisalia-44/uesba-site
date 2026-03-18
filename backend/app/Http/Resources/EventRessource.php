c4210542e38942ec0d7fddb9f3452cd00ff56f2a
Yobi <yobibah7295@gmail.com>
2026-03-18 20:36:29 +0000

    fix

--- backend/app/Http/Resources/EventRessource.php
+++ backend/app/Http/Resources/EventRessource.php
@@ -5,6 +5,8 @@ namespace App\Http\Resources;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+
 
 class EventRessource extends JsonResource
 {
@@ -16,14 +18,15 @@ class EventRessource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-                 'nom'=>$this->nom,
-        'descriptions'=>$this->descriptions,
-        'date_evenement'=>Carbon::parse($this->date_evenement)->locale('ALG'),
-        'type'=>$this->type,
-        'lieu'=>$this->lieu,
-        'photo'=>url($this->photo),
-        'estPasse'=>$this->isPast(),
-        'enCours'=>$this->isNow()
+            'id' => $this->id,
+            'nom' => $this->nom,
+            'descriptions' => $this->descriptions,
+            'date_evenement' => Carbon::parse($this->date_evenement)->locale('ALG'),
+            'type' => $this->type,
+            'lieu' => $this->lieu,
+            'photo' => $this->photo ? 'http://localhost:8000/storage/photo/'.$this->photo : null,
+            'estPasse' => $this->isPast(),
+            'enCours' => $this->isNow()
         ];
     }
 }
